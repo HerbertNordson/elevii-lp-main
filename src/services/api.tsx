bad1be3dbee99f5a899547e9b32a73e9a30bc62a
Herbert Nordson <herbertnordson@MacBook-Pro-de-Herbert.local>
2024-10-07 20:25:56 -0300

    api content

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -3,6 +3,6 @@ import axios from 'axios'
 export const api = axios.create({
   baseURL: 'https://elevii-lp-api.andersudev.workers.dev/api/',
   headers: {
-    'Content-Type': 'application/json'
+    "Content-Type": "text/plain",
   }
 })
